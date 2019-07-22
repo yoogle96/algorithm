06cf540a391c02cb7cb3593ceaeb2b4ee15588be
yoogle96 <dmltjs851@gmail.com>
2019-07-22 23:55:26 +0900

    Solved prob_17298

---
+++ 스택/prob_17298_오큰수/prob_17298.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <auto>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+	vector<int> v(n), ans(n);
+	for(int i=0; i<n; i++) cin >> v[i];
+
+	stack<int> stk; stk.push(1e9+7);
+	for(int i=n-1; i>=0; i--){
+		while(stk.top() <= v[i]) stk.pop();
+		if(stk.top() >= 1e9) ans[i] = -1;
+		else ans[i] = stk.top();
+		stk.push(v[i]);
+	}
+	for(auto i : ans) cout << i << " ";
+}
