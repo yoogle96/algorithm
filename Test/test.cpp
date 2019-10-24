41a33c5cc511f02ce60eb319d629390b5277957a
yoogle96 <dmltjs851@gmail.com>
2019-10-24 12:59:27 +0900

    fixed untracked files

--- Test/test.cpp
+++ Test/test.cpp
@@ -4,18 +4,6 @@
 using namespace std;
 
 int main(void){
-
-    vector<int> v;
-    for(int i = 0; i < 10; i++){
-        v.push_back(i);
-    }
-
-    v.clear();
-    cout << "------------" << endl;
-    for(int i = 0 ; i < v.size(); i++){
-        cout << v[i] << endl;
-    }
-    cout << v.size();
-    
+    cout << "TSET" << endl;
     return 0;
 }
