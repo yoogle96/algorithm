6b41c9061973dd4520d115950c9d4e5665d6476b
yoogle96 <dmltjs851@gmail.com>
2019-11-05 17:45:49 +0900

    2019.11.05 Backup

---
+++ 단계별/Step8_수학 1/1. prob_1712_손익분기점.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    long long int a, b, c, total = 0, ans=0, cur = 0;
+    scanf("%d %d %d", &a, &b, &c);
+    long long int bep = (a / (1 - )
+}
