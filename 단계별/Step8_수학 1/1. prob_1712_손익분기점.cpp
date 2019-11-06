19ea68cd79164e65e841069d5a24d6ac7ff4d72f
yoogle96 <dmltjs851@gmail.com>
2019-11-06 11:12:30 +0900

    Solve prob_1712

--- 단계별/Step8_수학 1/1. prob_1712_손익분기점.cpp
+++ 단계별/Step8_수학 1/1. prob_1712_손익분기점.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main(){
-    long long int a, b, c, total = 0, ans=0, cur = 0;
+    int a, b, c, ans=0;
     scanf("%d %d %d", &a, &b, &c);
-    long long int bep = (a / (1 - )
+    if(c <= b) ans = -1;
+    else ans = a / (c-b) + 1;
+    printf("%d", ans);
 }
