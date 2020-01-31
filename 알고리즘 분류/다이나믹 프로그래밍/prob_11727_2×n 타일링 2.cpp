9fe140c1d9aa7313c0b914cc1cfda84dc89b6994
yoogle96 <dmltjs851@gmail.com>
2020-01-31 22:43:38 +0900

    Solve prob_11727

--- 알고리즘 분류/다이나믹 프로그래밍/prob_11727_2×n 타일링 2.cpp
+++ 알고리즘 분류/다이나믹 프로그래밍/prob_11727_2×n 타일링 2.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
+int DP[1001];
+int N;
 int main() {
-    
-    
+    cin >> N;
+    DP[1] = 1;
+    DP[2] = 3;
+    for(int i = 3; i <= N; i++)
+        DP[i] = (DP[i - 1] + DP[i - 2] * 2) % 10007;
+    cout << DP[N];
 }
