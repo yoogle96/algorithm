ddd3d9d976004c44661d3c2e6a49f507fc91df0c
yoogle96 <dmltjs851@gmail.com>
2020-02-28 22:53:03 +0900

    Solve 71. 송아지 찾기(BFS : 상태트리탐색)

--- 인프런/71. 송아지 찾기(BFS : 상태트리탐색).cpp
+++ 인프런/71. 송아지 찾기(BFS : 상태트리탐색).cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+int S, E, x,pos, ch[10001], d[3] = {1, -1, 5};
 int main() {
-        
+    cin >> S >> E;
+    queue<int> q;
+    ch[S] = 1;
+    q.push(S);
+    while(!q.empty()) {
+        x = q.front();
+        q.pop();
+        for(int i = 0; i < 3; i++) {
+            pos = x + d[i];
+            if(pos < 1 && pos > 10000) continue;
+            if(pos == E) {
+                cout << ch[x];
+                return 0;
+            }
+            if(ch[pos] == 0) {
+                ch[pos] = ch[x] + 1;
+                q.push(pos);
+            }
+        }
+    }
+
 }
