3604b04c6a93d34c18e00e4b2b3b229222915088
yoogle96 <dmltjs851@gmail.com>
2020-03-15 02:32:40 +0900

    Solve [Groom Level]

--- 알고리즘 분류/다이나믹 프로그래밍/prob_2579_계단오르기.cpp
+++ 알고리즘 분류/다이나믹 프로그래밍/prob_2579_계단오르기.cpp
@@ -1,26 +1,37 @@
+//#include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <queue>
+
 using namespace std;
-
-int arr[301] = {0, };
-int dp[301] = {0, };
-
-
-int main(){
-    int n;
-    
+int n, dp[100001][100001];
+int main() {
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        cin >> arr[i];
+    vector< vector<int> > v((n * 2), vector<int>(n + 1, 0));
+    int idx = 1;
+    for(int i = 1; i <= (n * 2) - 1; i++) {
+        for(int j = 1; j <= idx; j++) {
+            cin >> v[i][j];
+        }
+        if(i >= n) idx--;
+        else idx++;
     }
-
-    dp[1] = arr[1];
-    dp[2] = dp[1] + arr[2];
-    dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
-
-    for(int i = 4; i <= n; i++){
-        dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i]);        
+    
+    dp[1][1] = v[1][1];
+    idx = 2;
+    for(int i = 2; i <= (n * 2) - 1; i++) {
+        for(int j = 1; j <= idx; j++) {
+            if(i > n) {
+                dp[i][j] = v[i][j] + max(dp[i - 1][j], dp[i - 1][j + 1]);
+            }else {
+                dp[i][j] = v[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1]);
+            }
+        }
+        if(i >= n) idx--;
+        else idx++;
     }
-
-    cout << dp[n] << endl;
+    
+    cout << dp[n * 2 - 1][1] << endl;
+    return 0;
 }
