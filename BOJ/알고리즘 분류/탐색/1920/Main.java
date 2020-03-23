ecd19c66057a2186ebcc19c7e6a82d764afb6fb4
yoogle96 <dmltjs851@gmail.com>
2020-03-24 01:40:45 +0900

    Solve [BOJ] 1920 java

---
+++ BOJ/알고리즘 분류/탐색/1920/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n, m;
+        Long num;
+        HashSet<Long> hashSet = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        for(int i = 0; i < n; i++) {
+            num = scanner.nextLong();
+            hashSet.add(num);
+        }
+        m = scanner.nextInt();
+        for(int i = 0; i < m; i++) {
+            num = scanner.nextLong();
+            if(hashSet.contains(num)) {
+                System.out.println(1);
+            }else {
+                System.out.println(0);
+            }
+        }
+    }
+}
