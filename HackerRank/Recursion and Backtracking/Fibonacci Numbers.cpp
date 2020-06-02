72584bb87f96d8ff6726ed239448e006d96c7551
Yu UiSeon <dmltjs851@gmail.com>
2020-06-03 04:02:27 +0900

    Solve [HackerRank] #Fibonacci Numbers .cpp
    - Recursion

---
+++ HackerRank/Recursion and Backtracking/Fibonacci Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+    if(n == 0 || n == 1) return n;
+    return fibonacci(n-2) + fibonacci(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
