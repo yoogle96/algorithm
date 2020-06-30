fa9851c93ee93b3a2d82864ce4c1200bdf5e01ba
Yu UiSeon <dmltjs851@gmail.com>
2020-07-01 00:54:15 +0900

    Solve [BOJ] #2805:나무자르기 .cpp
    - 이분탐색

--- BOJ/알고리즘 분류/이분탐색/2805.cpp
+++ BOJ/알고리즘 분류/이분탐색/2805.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
+long long n, m, ans;
+vector<long long> woods;
 int main() {
     cin >> n >> m;
-    
+    for(int i = 0; i < n; i++) {
+        long long tmp = 0;
+        cin >> tmp;
+        woods.push_back(tmp);
+    }
+    sort(woods.begin(), woods.end());
+    long long left = 0;
+    long long right = woods[n-1];
+    while(left <= right) {
+        long long mid = (left + right) / 2;
+        long long sum = 0;
+        for(int i = 0; i < n; i++) {
+            if(woods[i] <= mid) continue;
+            sum += woods[i] - mid;
+        }
+        
+        if(sum < m) {
+            right = mid - 1;
+        }else {
+            if(ans < mid) {
+                ans = mid;
+            }
+            left = mid + 1;
+        }
+    }
+    cout << ans << endl;
 }
