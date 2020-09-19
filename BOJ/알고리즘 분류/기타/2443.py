9bdf8ffab59b68b4e09c67191dc47b053614b367
dmltjs851 <dmltjs851@gmail.com>
2020-09-19 23:47:18 +0900

    Solve [BOJ] #2443 .py

--- BOJ/알고리즘 분류/기타/2443.py
+++ BOJ/알고리즘 분류/기타/2443.py
@@ -1,7 +1,7 @@
 n = int(input())
-for i in range(n, 0, -1):
-    for j in range(n - i):
-        print(' ', end='')
-    for k in range(i * 2 - 1):
-        print('*', end='')
-    print()
+for i in range(n):
+    print(' ' * (n-i-1), end='')
+    print('*'*(i*2+1))
+for i in range(1,n):
+    print(' ' * i, end='')
+    print('*'*((n*2)-(i*2+1)))
